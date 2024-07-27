d5397dfbbaa818befc875d5214f9038168bcb783
Muneer Hashmat <muneerhashmat@gmail.com>
2024-07-27 23:31:26 +0530

    fixed scroll issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CryptoDetails from "./pages/CryptoDetails";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
     <div className="bg-white dark:bg-[#181A20] text-black dark:text-white min-h-[100vh]">
       <Router>
         <Navbar />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/crypto/:cryptoId" element={<CryptoDetails />} />

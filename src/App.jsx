77aa8d608f902bbac01c33da77049244cc30d532
Muneer Hashmat <muneerhashmat@gmail.com>
2024-11-16 15:30:13 +0530

    removed toaster

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,9 @@ import Home from "./pages/Home";
 import CryptoDetails from "./pages/CryptoDetails";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
-import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <div className="bg-white dark:bg-[#181A20] text-black dark:text-white min-h-[100vh]">
-      <Toaster />
       <Router>
         <Navbar />
         <ScrollToTop />
